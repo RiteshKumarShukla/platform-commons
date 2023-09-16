42ea2ff66fc18f9e575d4a57540265bd58c56570
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-09-16 07:23:35 +0530

    Order total & no data in cart functionalities implemented

--- platfrom-commons/src/app/cart/cart.component.ts
+++ platfrom-commons/src/app/cart/cart.component.ts
@@ -20,4 +20,17 @@ export class CartComponent implements OnInit {
   removeFromCart(productId: number): void {
     this.cartService.removeFromCart(productId);
   }
+
+  calculateOrderTotal(): number {
+    return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  }
+
+  calculateEstimatedDeliveryDate(): string {
+    // Implement logic to calculate estimated delivery date here
+    return '2-3 days'; // Example: Replace with actual calculation
+  }
+
+  checkout(): void {
+    // Implement checkout logic here (e.g., navigate to a confirmation page)
+  }
 }
