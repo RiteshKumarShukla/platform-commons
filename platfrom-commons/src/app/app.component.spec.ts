6e847e4e8b1ee3d3ad9276627cedb87f3c9e6de5
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-09-16 07:04:54 +0530

    Fixed Everything in Catalogue Page

--- platfrom-commons/src/app/app.component.spec.ts
+++ platfrom-commons/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'platform-commons'`, () => {
+  it(`should have as title 'platfrom-commons'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('platform-commons');
+    expect(app.title).toEqual('platfrom-commons');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('platform-commons app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('platfrom-commons app is running!');
   });
 });
